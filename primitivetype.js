672dc44d4e59632c86855721d33995be5318f17b
minhajspace <minhaj.space@gmail.com>
2019-12-02 15:01:53 +0530

    Primitive types

---
+++ primitivetype.js
@@ -0,0 +1,30 @@
+//  Primitive or value type
+
+// staring
+
+// let name = "minhaj ";
+// console.log(`this is called string literal`);
+// console.log(name);
+
+// Number
+
+// let age = 21;
+// console.log(`this is called number literal`);
+// console.log(age);
+
+//  boolean
+
+// let isApproved = ture;
+// console.log(isApproved);
+
+// undifined
+
+// let Name = undefined;
+//  console.log(Name);
+
+// null
+
+// let selectedcolor = null;
+// console.log(selectedcolor);
+// selectedcolor = "red";
+// console.log(selectedcolor);
