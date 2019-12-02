123f0d1f06d02a8637cb6ff7717d75f1efaa3195
minhajspace <minhaj.space@gmail.com>
2019-12-02 18:53:00 +0530

    Arthmatic operators

---
+++ operator.js
@@ -0,0 +1,42 @@
+let x = 20;
+let y = 3;
+
+// // addtion
+// console.log(x + y);
+
+// // substraction
+
+// console.log(x - y);
+
+// // multiplication
+
+// console.log(x * y);
+
+// //  division
+
+// console.log(x / y);
+
+// // module
+
+// console.log(x % y);
+
+// console.log(x ** y);
+
+// increment
+// pre increment
+
+//console.log(++x);
+
+// post increment
+
+// console.log(x++);
+// console.log(x);
+
+// pre decrement
+
+// console.log(--x);
+
+// post decrement
+
+//console.log(x--);
+//console.log(x);
