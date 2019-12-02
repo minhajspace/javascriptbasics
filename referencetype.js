03fc31645e14e3e7109b96cc97061fe91596fed0
minhajspace <minhaj.space@gmail.com>
2019-12-02 15:11:48 +0530

    reference type

---
+++ referencetype.js
@@ -0,0 +1,20 @@
+let person = {
+  name: "vishal",
+  lastname: "anand",
+  age: 23,
+  city: "kanpur",
+  state: "Up"
+};
+
+console.log(person);
+
+// we can use these propety by useing these two method
+// Dot Notation
+
+person.name = "Minhaj ahmad khan ";
+console.log(person);
+
+// Bracket Notation
+
+person["name"] = "urooj";
+console.log(person);
