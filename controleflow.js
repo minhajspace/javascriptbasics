7e0d384a8eff41e3560cc5909c444b4884af22be
minhajspace <minhaj.space@gmail.com>
2019-12-03 15:04:27 +0530

    for loop

--- controleflow.js
+++ controleflow.js
@@ -1,24 +1,38 @@
-// if else
+// // if else
 // let houre = 13;
 
 // if (houre >= 6 && houre < 12) console.log("good morning");
 // else if (houre >= 12 && houre < 18) console.log("good afternoon");
 // else console.log("good evening");
-//                                 swtich case
 
-let weekend = "monday";
+// if (houre > 6 && houre < 12 ? "good morning" : "good evening ");
 
-switch (weekend) {
-  case "saturday":
-    console.log("this is the 2nd last day of the week ");
-    break;
-  case "sunday":
-    console.log("last day of the week");
-    break;
-  default:
-    console.log("given day is not weekends day ");
-} // we also do this on using if else statment
+// //                                 swtich case
 
-if (weekend === "saturday") console.log("this is the 2nd last day of the week");
-else if (weekend === "sunday") console.log("this is the last day of the week ");
-else console.log("this is not the weekends ");
+// let weekday = "sunday";
+
+// switch (weekday) {
+//   case "sunday":
+//     console.log("this is last day of the week ");
+//     break;
+//   case "saturday":
+//     console.log("this is seconday of the week ");
+//     break;
+//   default:
+//     console.log("thease are week day in between monday to friday ");
+// }
+
+// } // we also do this on using if else statment
+
+// if (weekend === "saturday") console.log("this is the 2nd last day of the week");
+// else if (weekend === "sunday") console.log("this is the last day of the week ");
+// else console.log("this is not the weekends ");
+//      for loops
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("hello world");
+// }
+
+for (let i = 4; i <= 40; i++) {
+  if (i % 4 === 0) console.log(i);
+}
