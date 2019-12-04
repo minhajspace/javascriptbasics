9e47c44d5cdddc89b860f55a4162980b2a49f057
minhajspace <minhaj.space@gmail.com>
2019-12-04 21:08:29 +0530

    factory function

--- objects.js
+++ objects.js
@@ -1,13 +1,33 @@
-const circel = {
-  radius: 1,
-  loaction: {
-    x: 1,
-    y: 1
-  },
-  inVisuble: true,
-  Addtion: function(a, b) {
-    return console.log(a + b);
-  }
-};
+// function masterCirclet() {}
+// circel = {
+//   radius: 1,
+//   loaction: {
+//     x: 1,
+//     y: 1
+//   },
+//   inVisuble: true,
+//   welcome: function() {
+//     return console.log("welcome to object oriented programming");
+//   }
+// };
 
-circel.Addtion(4, 4);
+// Factory function
+
+function masterfunction(radius, x, y) {
+  return {
+    radius,
+    loaction: {
+      x,
+      y
+    },
+    welcome() {
+      console.log("welcome to object oriented programming");
+    }
+  };
+}
+
+const circel = masterfunction(1, 2, 3);
+console.log(circel);
+
+const circel2 = masterfunction(1, 5, 6);
+console.log(circel2);
