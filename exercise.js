76fe70636c727aaf64c6ed900040c4e5fef5f792
minhajspace <minhaj.space@gmail.com>
2019-12-05 01:44:43 +0530

    2nd example

--- exercise.js
+++ exercise.js
@@ -1,11 +1,28 @@
-const address = {
-  street: "new hadier ganj ",
-  city: "Lucknow",
-  zipcode: 226003
-};
+// const address = {
+//   street: "new hadier ganj ",
+//   city: "Lucknow",
+//   zipcode: 226003
+// };
 
-function showAddress(address) {
-  for (let key in address) console.log(key, address[key]);
-  for (let key of Object.entries(address)) console.log(key);
+// function showAddress(address) {
+//   for (let key in address) console.log(key, address[key]);
+//   for (let key of Object.entries(address)) console.log(key);
+// }
+// showAddress(address);
+
+// function factoryfunction(street, city, zipcode) {
+//   return {
+//     street,
+//     city,
+//     zipcode
+//   };
+// }
+
+function ConstFucntion(street, city, zipcode) {
+  this.street = street;
+  this.city = city;
+  this.zipcode = zipcode;
 }
-showAddress(address);
+
+const newfunction = new ConstFucntion("newHaiderganj", " city", "226003");
+console.log(newfunction);
