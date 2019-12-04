7ca1239ea6e9288d50ae17cc341572e68918466c
minhajspace <minhaj.space@gmail.com>
2019-12-04 22:47:57 +0530

    constructor function

--- objects.js
+++ objects.js
@@ -13,21 +13,38 @@
 
 // Factory function
 
-function masterfunction(radius, x, y) {
-  return {
-    radius,
-    loaction: {
-      x,
-      y
-    },
-    welcome() {
-      console.log("welcome to object oriented programming");
-    }
+// function masterfunction(radius, x, y) {
+//   return {
+//     radius,
+//     isApproved: true,
+//     loaction: {
+//       x,
+//       y
+//     },
+//     welcome() {
+//       console.log("welcome to object oriented programming");
+//     }
+//   };
+// }
+
+// const circel = masterfunction(1, 2, 3);
+// console.log(circel);
+
+// const circel2 = masterfunction(1, 5, 6);
+// console.log(circel2);
+//                            Constructor function
+
+function ConstructorFunction(radius, x, y) {
+  this.radius = radius;
+  (this.location = {
+    x: x,
+    y: y
+  }),
+    (this.isApproved = true);
+  this.draw = function() {
+    console.log("draw");
   };
 }
 
-const circel = masterfunction(1, 2, 3);
+const circel = new ConstructorFunction(1, 4, 5);
 console.log(circel);
-
-const circel2 = masterfunction(1, 5, 6);
-console.log(circel2);
