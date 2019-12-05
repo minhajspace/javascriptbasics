cfa19a1412eb11ca98fdc93425186d3084012349
minhajspace <minhaj.space@gmail.com>
2019-12-05 15:30:59 +0530

    how primitive and refrence type behave defferently

--- exercise.js
+++ exercise.js
@@ -30,23 +30,49 @@
 
 // factory function
 
-function createObject(radius) {
-  return {
-    radius,
-    draw: function() {
-      console.log("draw");
-    }
-  };
-}
-const newfunction = createObject(1);
-console.log(newfunction);
-// constructor function
-function ObjectCreate(radius) {
-  this.radius = radius;
-  this.draw = function() {
-    console.log("draw");
-  };
-}
+// function createObject(radius) {
+//   return {
+//     radius,
+//     draw: function() {
+//       console.log("draw");
+//     }
+//   };
+// }
+// const newfunction = createObject(1);
+// console.log(newfunction);
+// // constructor function
+// function ObjectCreate(radius) {
+//   this.radius = radius;
+//   this.draw = function() {
+//     console.log("draw");
+//   };
+// }
 
-const another = new ObjectCreate(1);
-console.log(another);
+// const another = new ObjectCreate(1);
+// console.log(another);
+
+// every functions Objects in JavaScript
+
+// function ConsturtorFunction(radius) {
+//   this.radius = radius;
+//   this.greeting = function() {
+//     console.log("good afternoon");
+//   };
+// }
+
+// const NewCircel = new ConsturtorFunction(1);
+// ConsturtorFunction.call({}, 5);
+
+// how primitive and Refrence type behave differently
+
+let x1 = { value: 10 };
+let y1 = x1;
+x1.value = 20;
+
+let number = 10;
+
+function increased(number) {
+  number++;
+}
+increased(number);
+console.log(number);
