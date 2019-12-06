05735afc87c594cb0f33a8ecfbe177547c56d770
minhajspace <minhaj.space@gmail.com>
2019-12-06 12:46:14 +0530

    value and object type string

--- objects.js
+++ objects.js
@@ -81,12 +81,12 @@
 //   };
 // }
 
-const Circel = {
-  radius: "radius",
-  greeting() {
-    console.log("good evening ");
-  }
-};
+// const Circel = {
+//   radius: "radius",
+//   greeting() {
+//     console.log("good evening ");
+//   }
+// };
 
 // for (let key in Circel) console.log(key, Circel[key]);
 // for (let key of Object.keys(Circel)) console.log(key);
@@ -113,5 +113,121 @@ const Circel = {
 //   ...Circel
 // };
 // console.log(newObject);
+//const newfunction = new ConstFucntion("newHaiderganj", " city", "226003");
 
-// excersice
+//  Constructor Properties
+
+// factory function
+
+// function createObject(radius) {
+//   return {
+//     radius,
+//     draw: function() {
+//       console.log("draw");
+//     }
+//   };
+// }
+// const newfunction = createObject(1);
+// console.log(newfunction);
+// // constructor function
+// function ObjectCreate(radius) {
+//   this.radius = radius;
+//   this.draw = function() {
+//     console.log("draw");
+//   };
+// }
+
+// const another = new ObjectCreate(1);
+// console.log(another);
+
+// every functions Objects in JavaScript
+
+// function ConsturtorFunction(radius) {
+//   this.radius = radius;
+//   this.greeting = function() {
+//     console.log("good afternoon");
+//   };
+// }
+
+// const NewCircel = new ConsturtorFunction(1);
+// ConsturtorFunction.call({}, 5);
+
+// how primitive and Refrence type behave differently
+
+// let x1 = { value: 10 };
+// let y1 = x1;
+// x1.value = 20;
+
+// let number = 10;
+
+// function increased(number) {
+//   number++;
+// }
+// increased(number);
+// console.log(number);
+//         enumerating
+// const circel = {
+//   radius: 1,
+//   draw: function() {
+//     console.log("draw");
+//   }
+// };
+
+// for (let key in circel) console.log(key);
+
+// for (let key of Object.keys(circel)) console.log(key);
+
+// for (let entey of Object.entries(circel)) console.log(entey);
+
+// "radius" in circel;
+// console.log("yes");
+
+// Cloning an object
+// Old method
+
+// const circel = {
+//   radius: 1,
+//   draw() {
+//     console.log("draw");
+//   }
+// };
+// const another = {};
+// for (let key in circel) another[key] = circel[key];
+// console.log(another);
+
+// using Object.assign
+// const another = Object.assign({}, circel);
+// console.log(another);
+// using spread operator
+// const another = { ...circel };
+// console.log(another);
+
+// string
+
+//Primitive type
+
+let message = " hello ";
+
+console.log(message);
+
+//Object type string
+
+let another = new String("h1");
+console.log(another);
+
+//   String Built in Method
+//message = "   hi hello  how are you  ?    ";
+// console.log(message.length);
+// console.log(message[0]);
+// console.log(message[2]);
+// console.log(message.startsWith("h"));
+// console.log(message.includes("hi"));
+// console.log(message.endsWith("r"));
+// console.log(message.indexOf("how"));
+// console.log(message.toUpperCase());
+// console.log(message.toLowerCase());
+// console.log(message.trim());
+// console.log(message.trimLeft());
+// console.log(message.trimRight());
+
+// console.log(message.replace("hello", "everyone"));
