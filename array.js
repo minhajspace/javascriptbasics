c36836dfd69c9a3b17a6d4b285e0f35ffe028ee3
minhajspace <minhaj.space@gmail.com>
2019-12-06 20:29:19 +0530

    includes method

--- array.js
+++ array.js
@@ -3,7 +3,7 @@
 // .splice
 // .unshift
 // .splice
-const number = [1, 6, 7, 1];
+const number = [1, 3, 6, 7, 1];
 
 console.log(number);
 number.push(8, 9);
@@ -12,7 +12,15 @@ number.unshift(3, 4);
 console.log(number);
 number.splice(3, 0, "a", "b");
 console.log(number);
+
 // finding element in Array
 console.log(number.indexOf(1));
 console.log(number.indexOf(4, 1));
 console.log(number.lastIndexOf("c"));
+// for finding the element exist in array or not
+
+console.log(number.indexOf(2) !== -1);
+
+// for the same work we have one more more method which more elegent
+
+console.log(number.includes(1, 1));
