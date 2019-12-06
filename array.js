bdb8a5e7360dd1ee0e8ef3bcac50f1c011aaada2
minhajspace <minhaj.space@gmail.com>
2019-12-06 21:26:09 +0530

    finding array in Refrence type

--- array.js
+++ array.js
@@ -13,7 +13,8 @@ console.log(number);
 number.splice(3, 0, "a", "b");
 console.log(number);
 
-// finding element in Array
+// finding element in  Primitive Type Array
+
 console.log(number.indexOf(1));
 console.log(number.indexOf(4, 1));
 console.log(number.lastIndexOf("c"));
@@ -24,3 +25,34 @@ console.log(number.indexOf(2) !== -1);
 // for the same work we have one more more method which more elegent
 
 console.log(number.includes(1, 1));
+
+// Now Finding Element in Refrence type Array
+
+const courses = [
+  { id: 1, name: "Btech" },
+  { id: 2, name: "Bsc" },
+  { id: 3, name: "Ba" },
+  { id: 4, name: "Bca " }
+];
+
+const result1 = courses.find(result1 => {
+  return result1.name === "Btech";
+});
+console.log(result1);
+
+const arrayNumber = [20, 25, 40, 36, 78];
+
+const result = arrayNumber.find(result => {
+  return result > 20 && result <= 40;
+});
+console.log(result);
+
+const number2 = [
+  { id: 1, value: "a" },
+  { id: 2, value: "b" }
+];
+
+const anotherResult = number2.find(anotherResult => {
+  return anotherResult.value === "a";
+});
+console.log(anotherResult);
