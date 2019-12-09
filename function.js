ac3b282c5599de8484b2f48158944f6d9b0e126a
minhajspace <minhaj.space@gmail.com>
2019-12-09 19:07:14 +0530

    rest Operator

--- function.js
+++ function.js
@@ -1,60 +1,77 @@
-// In javaScript we have two Method to Define a Fucntion
+// // In javaScript we have two Method to Define a Fucntion
 
-//   (1)  Function Declaration
-//   (2)  Function Expression
+// //   (1)  Function Declaration
+// //   (2)  Function Expression
 
-// function Declaration
+// // function Declaration
 
-// We can call a Fucntion that is Defined using the Function declaration +
-// Before its Defined  .
-// BUT WHY IS THAT
+// // We can call a Fucntion that is Defined using the Function declaration +
+// // Before its Defined  .
+// // BUT WHY IS THAT
 
-//  The reason for this is because when our JavaScript engine executes +
-//  this code, it moves all the function declarations to the  +
-//  top, so our code will look like this at run time.  +
+// //  The reason for this is because when our JavaScript engine executes +
+// //  this code, it moves all the function declarations to the  +
+// //  top, so our code will look like this at run time.  +
 
-// SO OUR CODE WILL LOOK LIKE THIS AT RUN TIME
+// // SO OUR CODE WILL LOOK LIKE THIS AT RUN TIME
+
+// // function walk() {
+// //     console.log("walk");
+// // }
+
+// // walk();
+
+// // What is Hoisting ?
+
+// // hoisting the process to to moving function Decelaration to the top of the file +
+// // and this is done automatically by the javascript engine that is executing this code
+
+// //  In contrast we dont put semicolon in the end of the function it really doesnt matter by but convention we never do that
 
 // walk();
+
 // function walk() {
-//     console.log("walk");
+//   console.log("walk");
 // }
 
-// walk();
+// // Function Expression
 
-// What is Hoisting ?
+// // At the end here we need to add a semicolon because in javascript
+// // when ever we declare a variable or constant we terminate this statment with semicolon
 
-// hoisting the process to to moving function Decelaration to the top of the file +
-// and this is done automatically by the javascript engine that is executing this code
+// // Named Fuction Expression
 
-//  In contrast we dont put semicolon in the end of the function it really doesnt matter by but convention we never do that
+// let run = function run() {
+//   console.log("run");
+// };
 
-walk();
+// run();
 
-function walk() {
-  console.log("walk");
+// // Anonymus Function Expression
+
+// let runFast = function() {
+//   console.log("run fast");
+// };
+// runFast();
+
+// //  here runFast and move both refrence the same Anonymus function Expression
+// move = runFast;
+// move();
+
+// Arguments
+
+// function sum() {
+//   sum = 0;
+//   for (let element of arguments) sum += element;
+//   return sum;
+// }
+// console.log(sum(12, 2, 2, 3, 32, 6, 1));
+
+//    Rest operator
+
+function sum(distun, ...argument) {
+  let sum = argument.reduce((a, b) => a + b);
+  return sum;
 }
 
-// Function Expression
-
-// At the end here we need to add a semicolon because in javascript
-// when ever we declare a variable or constant we terminate this statment with semicolon
-
-// Named Fuction Expression
-
-let run = function run() {
-  console.log("run");
-};
-
-run();
-
-// Anonymus Function Expression
-
-let runFast = function() {
-  console.log("run fast");
-};
-runFast();
-
-//  here runFast and move both refrence the same Anonymus function Expression
-move = runFast;
-move();
+console.log(sum(5, 20, 40, 40));
