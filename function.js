a9e74f50d76f8397a5ead0197b1b3044b0b725aa
minhajspace <minhaj.space@gmail.com>
2019-12-22 23:32:22 +0530

    web works

--- function.js
+++ function.js
@@ -432,18 +432,30 @@ sum(4);
 
 //  purpose of race method in promise
 
-new Promise(function(resolve, reject) {
-  setTimeout(() => resolve(1), 1000);
-})
-  .then(function(result) {
-    console.log(result); // 1
-    return result * 2;
-  })
-  .then(function(result) {
-    console.log(result); // 2
-    return result * 3;
-  })
-  .then(function(result) {
-    console.log(result); // 6
-    return result * 4;
-  });
+// new Promise(function(resolve, reject) {
+//   setTimeout(() => resolve(1), 1000);
+// })
+//   .then(function(result) {
+//     console.log(result); // 1
+//     return result * 2;
+//   })
+//   .then(function(result) {
+//     console.log(result); // 2
+//     return result * 3;
+//   })
+//   .then(function(result) {
+//     console.log(result); // 6
+//     return result * 4;
+//   });
+
+// web works
+
+let i = 0;
+
+function timedCount() {
+  i = i + 1;
+  postMessage(i);
+  setTimeout("timedCount()", 500);
+}
+
+timedCount();
