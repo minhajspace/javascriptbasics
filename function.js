00010f5aed2bcabdedb67edac5001c8850d60545
minhajspace <minhaj.space@gmail.com>
2019-12-22 23:31:12 +0530

    purpose of race method in promise

--- function.js
+++ function.js
@@ -414,6 +414,24 @@ sum(4);
 
 // Promish changing
 
+// new Promise(function(resolve, reject) {
+//   setTimeout(() => resolve(1), 1000);
+// })
+//   .then(function(result) {
+//     console.log(result); // 1
+//     return result * 2;
+//   })
+//   .then(function(result) {
+//     console.log(result); // 2
+//     return result * 3;
+//   })
+//   .then(function(result) {
+//     console.log(result); // 6
+//     return result * 4;
+//   });
+
+//  purpose of race method in promise
+
 new Promise(function(resolve, reject) {
   setTimeout(() => resolve(1), 1000);
 })
