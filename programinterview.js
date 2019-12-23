983fc6caf4d76dc7de74e91c83636172cc1ef6d7
minhajspace <minhaj.space@gmail.com>
2019-12-23 22:10:01 +0530

    Recursively print all permutations of a string

--- programinterview.js
+++ programinterview.js
@@ -11,15 +11,38 @@
 // console.log(palindrome("A man, a plan, a canal. Panama"));
 // for loop
 
-function palindrome(str) {
-  var re = /[^A-Za-z0-9]/g;
-  str = str.toLowerCase().replace(re, "");
-  var len = str.length;
-  for (var i = 0; i < len / 2; i++) {
-    if (str[i] !== str[len - 1 - i]) {
-      return false;
+// function palindrome(str) {
+//   var re = /[^A-Za-z0-9]/g;
+//   str = str.toLowerCase().replace(re, "");
+//   var len = str.length;
+//   for (var i = 0; i < len / 2; i++) {
+//     if (str[i] !== str[len - 1 - i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// palindrome("A man, a plan, a canal. Panama");
+
+//  Recursively print all permutations of a string
+
+function getPermutations(string) {
+  var results = [];
+
+  if (string.length === 1) {
+    results.push(string);
+    return results;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    let firstChar = string[i];
+    let otherChar = string.substring(0, i) + string.substring(i + 1);
+    let otherPermutations = getPermutations(otherChar);
+
+    for (var j = 0; j < otherPermutations.length; j++) {
+      results.push(firstChar + otherPermutations[j]);
     }
   }
-  return true;
+  return results;
 }
-palindrome("A man, a plan, a canal. Panama");
+console.log(getPermutations("hello"));
