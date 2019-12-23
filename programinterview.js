930bf8529ca30b7621ba2d83f2780cd57835b990
minhajspace <minhaj.space@gmail.com>
2019-12-23 22:22:31 +0530

    final commit

--- programinterview.js
+++ programinterview.js
@@ -91,8 +91,11 @@ function firstNonRepeatedCharacter(string) {
   for (let i = 0; i < string.length; i++) {
     let c = string.charAt(i);
     if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
+      console.log(c);
       return c;
     }
   }
   return null;
 }
+
+firstNonRepeatedCharacter("hello");
