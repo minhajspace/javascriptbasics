0e0f0c0b45012d276e47369ca7eb6e07d06fce28
minhajspace <minhaj.space@gmail.com>
2020-01-14 01:08:21 +0530

    prime numer

--- array.js
+++ array.js
@@ -28,43 +28,43 @@
 
 // // // // Now Finding Element in Refrence type Array
 
-const courses = [
-    { id: 1, name: "Btech" },
-    { id: 2, name: "Bsc" },
-    { id: 3, name: "Ba" },
-    { id: 4, name: "Bca " }
-];
+// const courses = [
+//     { id: 1, name: "Btech" },
+//     { id: 2, name: "Bsc" },
+//     { id: 3, name: "Ba" },
+//     { id: 4, name: "Bca " }
+// ];
 
-const result1 = courses.find(function (result1) {
-    return result1.name === "Btech";
-});
-console.log(result1);
+// const result1 = courses.find(function (result1) {
+//     return result1.name === "Btech";
+// });
+// console.log(result1);
 
-const arrayNumber = [20, 25, 40, 36, 78];
+// const arrayNumber = [20, 25, 40, 36, 78];
 
-const result = arrayNumber.find(function (result) {
-    return result > 20 && result <= 40;
-});
-console.log(result);
+// const result = arrayNumber.find(function (result) {
+//     return result > 20 && result <= 40;
+// });
+// console.log(result);
 
-const number2 = [
-    { id: 1, value: "a" },
-    { id: 2, value: "b" }
-];
+// const number2 = [
+//     { id: 1, value: "a" },
+//     { id: 2, value: "b" }
+// ];
 
-const anotherResult = number2.find(anotherResult => {
-    return anotherResult.value === "a";
-});
-console.log(anotherResult);
+// const anotherResult = number2.find(anotherResult => {
+//     return anotherResult.value === "a";
+// });
+// console.log(anotherResult);
 
 // // //                       arrow function
 
-const newnumber = [1, 5, 6, 8, 12];
+// const newnumber = [1, 5, 6, 8, 12];
 
-const result = newnumber.find(result => {
-    return result > 5;
-});
-console.log(result);
+// const result = newnumber.find(result => {
+//     return result > 5;
+// });
+// console.log(result);
 
 // // // //                    Removing element from array
 // // // //  method remove element form the end of the array
@@ -453,11 +453,14 @@ console.log(result);
 
 // console.log(getMissing("1 2 3 5 7 8"));
 
-let num1;
 function isPrime(num) {
-    if (num % num == 0)
-        num1 = num
-    console.log(num1)
-    return num1
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return (` ${num} number is not a prime number`)
+        }
+        return (`${num} number is a prime number`)
+    }
+
 }
-console.log(isPrime(4))
+
+console.log(isPrime(3))
