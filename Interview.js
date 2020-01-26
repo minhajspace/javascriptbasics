c616b66d764ec79bbe9fbabe53aebea8b3f6446f
minhajspace <minhaj.space@gmail.com>
2020-01-26 17:43:53 +0530

    clouser

--- Interview.js
+++ Interview.js
@@ -32,6 +32,32 @@
 
 // example  
 
-(function Sum(num, num2) {
-    return num + num2
-})(20, 30)           // answer will be 50 
+// (function Sum(num, num2) {
+//     return num + num2
+// })(20, 30)           // answer will be 50 
+
+
+// what is clouser   
+
+// Answer   A Clouser is An inner Function that has Access to the scope of outher function  
+//  A Clouser have access  to variable in 3 seprate scope 
+// 1  its own scope 
+//   2  Outer function scope 
+//  3   Global variable scope  
+
+// A clouser have Also access to 
+//  (1) Its own parameters  
+//  (2)  Outer Function 
+
+// example    
+
+function outer(para1) {
+    let n = 1;
+    function inner() {
+        let m = 5;
+        let result = n + m
+        return (para1 + (n + m))
+    }
+    console.log(inner())
+}
+outer("Sum of the two number is  ")
