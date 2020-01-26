f39f7bd889f86593f0d04fe81b0f1a0550407bdf
minhajspace <minhaj.space@gmail.com>
2020-01-26 16:37:08 +0530

    IIFE

--- Interview.js
+++ Interview.js
@@ -4,21 +4,34 @@
 
 // Example of Currying 
 
-function addThreeDigit(num) {
-    return function (num2) {
-        return function (num3, num4) {
-            return num + num2 + num3 + num4
-        }
-    }
-}
+// function addThreeDigit(num) {
+//     return function (num2) {
+//         return function (num3, num4) {
+//             return num + num2 + num3 + num4
+//         }
+//     }
+// }
 
-console.log(addThreeDigit((20)(30)(50, 10)))
+// console.log(addThreeDigit((20)(30)(50, 10)))
 
-// still this is curring function 
+// // still this is curring function 
 
-// without curring option 
+// // without curring option 
 
-function sum(num, num1, num2) {
-    return num + num1 + num2
-}
-console.log(sum(20, 30, 20))
+// function sum(num, num1, num2) {
+//     result = num + num1 + num2
+//     return result;
+// }
+// console.log(sum(20, 30, 20))
+
+
+
+// What is IIFY and why they use ? 
+
+// IIFY stand for Immediately invoked Function Expression its simply a function that excuted right after its created 
+
+// example  
+
+(function Sum(num, num2) {
+    return num + num2
+})(20, 30)           // answer will be 50 
