b9eac4dc4ff626f16db0dabea5e1249df67e3e5a
minhajspace <minhaj.space@gmail.com>
2020-01-27 15:01:40 +0530

    4 pillars of oops

--- oops.js
+++ oops.js
@@ -1,4 +1,4 @@
-//                               OOPS 
+//                               OOPS
 
 // What is OOPS  ? 
 
@@ -12,25 +12,38 @@
 
 // example without using incapsulation 
 
-let salary = 3000;
-let overtime = 10;
-let cost = 1000;
+// let salary = 3000;
+// let overtime = 10;
+// let cost = 1000;
 
-getToatalSalary = (salary = 3000, overtime = 10, cost = 1000) => {
-    return (salary + (overtime * cost))
-}
-console.log(getToatalSalary())
+// getToatalSalary = (salary = 3000, overtime = 10, cost = 1000) => {
+//     return (salary + (overtime * cost))
+// }
+// console.log(getToatalSalary())
 
-// now using using  encapsulation 
+// // now using using  encapsulation 
 
-let employee = {
-    salary: 30000,
-    overtime: 10,
-    cost: 1000,
-    getToatalSalary() {
-        return (salary + (overtime * cost))
-    }
-}
-console.log(employee.getToatalSalary())
+// let employee = {
+//     salary: 30000,
+//     overtime: 10,
+//     cost: 1000,
+//     getToatalSalary() {
+//         return (salary + (overtime * cost))
+//     }
+// }
+// console.log(employee.getToatalSalary())
 
+
+// (2)         Abstraction  
+
+// Abstraction is Process of hiding Internal  and Showing functnolity 
+
+
+// (3)         Inheritance 
+
+// Inheritance is machinsm that allows you to eliminate redendent code  
+
+// (4)         Polymarphism 
+
+//  Polymarphism is technique that allow you to get rid of long if and else and ewitchan case statment
 
